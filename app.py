975336565956e71c5b93d6eccb82346dc33dde8b
Aryan Jain <aryan.one0793@gmail.com>
2024-11-29 00:34:59 +0530

    keyvault

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from firebase_admin import credentials
 from flask_cors import CORS
 from utils.decorators import protected_route
 
+import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
@@ -15,7 +19,7 @@ cred = credentials.Certificate(app.config['FIRESTORE_CREDENTIALS'])
 firebase_admin.initialize_app(cred)
 
 # Initialize SocketIO
-socketio = SocketIO(app, cors_allowed_origins="*")
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
 from controllers.user_controller import user_bp
 from controllers.driver_controller import driver_bp
@@ -34,10 +38,20 @@ app.register_blueprint(face_bp)
 app.register_blueprint(history_bp)
 
 
+keyVaultName = os.environ["KEY_VAULT_NAME"]
+KVUri = f"https://googlecreds.vault.azure.net"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+retrieved_secret = client.get_secret('googleCredsJson')
+
+
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "Hello World"
+    # return "Hello World"x
+    return retrieved_secret.value
 
 @app.route("/protected", methods=['GET'])
 @protected_route
@@ -45,5 +59,6 @@ def protected():
     return jsonify({"user": request.user}), 200
 
 if __name__ == '__main__':
-    # app.run(port=6001, debug=True)
-    socketio.run(app, host="0.0.0.0", port=6001, debug=True)
+    app.run()
+    # socketio.run(app, host="0.0.0.0", port=6001, debug=True)
+
